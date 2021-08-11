4a8571ea75da185e82a265ffdd85768ee81fc66a
Tarek Ahamed <64723695+TarekAhamed-LIV3D@users.noreply.github.com>
2021-08-11 23:18:23 +0600

    Created button and link with page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import './widgets/new_transaction.dart';
+
 import './widgets/user_transaction.dart';
 import 'package:flutter/material.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -16,27 +19,48 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   final titleController = TextEditingController();
   final amountController = TextEditingController();
+
+  void startAddNewTransaction(BuildContext ctx) {
+    showModelBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return NewTransaction(addTx);
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Center(child: Text('Track Your Expenses')),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.add),
+          )
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                width: double.infinity,
-                child: Card(
-                  color: Colors.blue,
-                  child: Text('Chart!'),
-                  elevation: 5,
-                ),
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('Chart!'),
+                elevation: 5,
               ),
-              UserTransactions()
-            ],
-          ),
+            ),
+            UserTransactions()
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
       ),
     );
   }
